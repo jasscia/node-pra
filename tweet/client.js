ae09862b139c9af4b6bc9098fc9c703e250d3304
Jassica <1648511816@qq.com>
2018-05-13 18:37:01 +0800

    tweet

--- tweet/client.js
+++ tweet/client.js
@@ -1,18 +1,29 @@
 var http=require('http')
 var qs=require('querystring')
-client=http.request({
-  host:'127.0.0.1',
-  port:3000,
-  url:'/',
-  method:'GET'
-},function(res){
-  var body='';
-  res.setEncoding('utf8'),
-  res.on('data',function(chunk){
-    body+=chunk
-  })
-  res.on('end',function(){
-    console.log('\n we got \033[96m' +body+ '\033[39m\n')
-  })
+
+function send(thename){
+  http.request({
+    host:'127.0.0.1',
+    port:3000,
+    url:'/',
+    method:'GET'
+  },function(res){
+    res.setEncoding('utf8')
+    var body='';
+    res.on('data',function(chunk){
+      body+=chunk
+      console.log(body)
+    })
+    res.on('end',function(){
+      console.log('\n  \033[90m request complete!\033[39m\n')
+      process.stdout.write('\n your name: ')
+    })
+  }).end(qs.stringify({name:thename}))
+}
+process.stdout.write('\n enter your name: ')
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+process.stdin.on('data',function(name){
+  console.log('before send',qs.stringify({name:name.replace('\n','')}))
+  send(name.replace('\n',''))
 })
-client.end()
--- tweet/server.js
+++ tweet/server.js
@@ -1,9 +1,17 @@
 var http=require('http')
 var qs=require('querystring')
 server=http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/html'})
-  res.end('hello world!')
-})
-server.listen('3000',function(){
-  console.log('listening on:3000')
+  var body="";
+  req.on('data',function(chunk){
+    body+=chunk;
+    console.log('i get:',body)
+  })
+  req.on('end',function(data){
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.end('Done!')
+
+    console.log('i got:',body,'--',data)
+    console.log('\n got name \033[90m' + qs.parse(body).name+'\033[39m\n')
+  })
+  
 })
